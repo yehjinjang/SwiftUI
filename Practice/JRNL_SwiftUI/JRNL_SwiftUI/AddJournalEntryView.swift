fa39099fb237f6546781992ed4ef353e77e4a322
yehjinjang <j95033176@gmail.com>
2024-05-28 16:38:11 +0900

    upate image,rating func

---
+++ Practice/JRNL_SwiftUI/JRNL_SwiftUI/AddJournalEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  AddJournalEntryView.swift
+//  JRNL_SwiftUI
+//
+//  Created by 장예진 on 5/28/24.
+//
+
+import SwiftUI
+
+struct AddJournalEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddJournalEntryView()
+}
