cf093e34d049de57acbac18bea6d731cb09df932
yehjinjang <j95033176@gmail.com>
2024-06-19 10:08:16 +0900

    delete

---
+++ server/07-AsyncCombine/BookShelf/BookShelf/BookDetailView.swift
@@ -0,0 +1,40 @@
+//
+//  BookDetailView.swift
+//  BookShelf
+//
+//  Created by Jungman Bae on 6/17/24.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    @Binding var book: Book
+    @State var showEditBookView = false
+    var body: some View {
+        Form {
+            Text(book.title)
+            Image(book.largeCoverImageName)
+                .resizable()
+                .scaledToFit()
+                .shadow(radius: 10)
+                .padding()
+            Button(action: { showEditBookView.toggle() }) {
+                Label("Edit", systemImage: "pencil")
+            }
+            Label(book.author, systemImage: "person.crop.rectangle")
+            Label("ISBN: \(book.isbn)", systemImage: "number")
+            Label("\(book.pages) pages", systemImage: "book")
+            Toggle("Read", isOn: .constant(book.isRead))
+        }
+        .navigationTitle(book.title)
+        .sheet(isPresented: $showEditBookView) {
+            BookEditView(book: $book)
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        BookDetailView(book: .constant(Book.sampleBooks[0]))
+    }
+}
---
+++ server/07-AsyncCombine/SimpleView/SimpleView/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  SimpleView
+//
+//  Created by Jungman Bae on 6/17/24.
+//
+
+import SwiftUI
+
+private class PersonViewModel: ObservableObject {
+    @Published var firstName = ""
+    @Published var lastName = ""
+    
+    func save() {
+        print("Save to disk")
+    }
+}
+
+struct ContentView: View {
+    @State var dirty = false
+    @StateObject private var viewModel = PersonViewModel()
+    
+    var body: some View {
+        Form {
+            Section("\(self.dirty ? "* " : "")Input fields") {
+                TextField("First name", text: $viewModel.firstName)
+                    .onChange(of: viewModel.firstName) {
+                        self.dirty = true
+                    }
+                TextField("Last name", text: $viewModel.lastName)
+                    .onChange(of: viewModel.lastName) {
+                        self.dirty = true
+                    }
+            }
+            .onSubmit {
+                viewModel.save()
+                self.dirty = false
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
