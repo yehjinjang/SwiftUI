de56386d4d413eae888d1c60f0ef3046e9a28367
yehjinjang <j95033176@gmail.com>
2024-06-20 13:09:30 +0900

    생성자 함수에서 변수와 스트림 연결

--- 07-AsyncCombine/SignUpForm2/SignUpForm2/SignUpViewModel.swift
+++ 07-AsyncCombine/SignUpForm2/SignUpForm2/SignUpViewModel.swift
@@ -9,26 +9,39 @@ import Foundation
 import Combine
 
 class SignUpFormViewModel: ObservableObject {
-    typealias Available = Result<Bool,Error>
+    typealias Available = Result<Bool, Error>
     
     @Published var username: String = ""
     @Published var usernameMessage: String = ""
     @Published var isValid: Bool = false
     @Published var showUpdateDialog: Bool = false
     
-    
     private var authenticationService = AuthenticationService()
     
-    private lazy var isUserNameAvailablePublisher : AnyPublisher<Bool, Never> = {
+    private lazy var isUsernameAvailablePublisher: AnyPublisher<Available, Never> = {
         $username
             .debounce(for: 0.5, scheduler: RunLoop.main)
             .removeDuplicates()
-            .flatMap{ username -> AnyPublisher<Bool, Never> in
+            .flatMap { username -> AnyPublisher<Available,Never> in
                 self.authenticationService.checkUserNameAvailablePublisher(userName: username)
                     .asResult()
-                
             }
+            .receive(on: DispatchQueue.main)
+            .print("before share")
             .share()
+            .print("share")
             .eraseToAnyPublisher()
     }()
+    
+    init() {
+        isUsernameAvailablePublisher.map { result in
+            switch result {
+            case .success(let isAvailable):
+                return isAvailable
+            case .failure(_):
+                return false
+            }
+        }
+        .assign(to: &$isValid)
+    }
 }
